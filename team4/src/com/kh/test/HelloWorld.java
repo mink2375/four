426a5545171488a443fab4991535c57548e3b30d
Ryu-sw <Ryu-sw@DESKTOP-2TK623G>
2020-06-04 14:25:02 +0900

    제대로 되나요?

--- team4/src/com/kh/test/HelloWorld.java
+++ team4/src/com/kh/test/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("잘되나요?");
+		System.out.println("맞나요??");
+		
 	}
 
 }
